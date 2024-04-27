e3466ae80a5afa61f248be7a0ff4d4f6e53df380
Sanjay-10z <sanjaysjs2003@gmail.com>
2024-04-27 23:06:10 +0530

    Files added

---
+++ README.md
@@ -0,0 +1,46 @@
+GreatQR - QR Code Generator
+This is a simple web application built with React for generating QR codes dynamically based on user input.
+
+Features
+Dynamic QR Code Generation: Users can input data and specify the size to generate QR codes dynamically.
+Download: Generated QR codes can be downloaded as PNG images.
+Static QR Code: The application also displays a static QR code image imported from an asset file.
+Responsive Design: The application is designed to be responsive and works well on different screen sizes.
+Technologies Used
+React
+JavaScript
+HTML
+CSS
+Installation
+Clone the repository:
+
+bash
+Copy code
+git clone 
+Navigate to the project directory:
+
+bash
+Copy code
+cd qr-code-generator
+Install dependencies:
+
+bash
+Copy code
+npm install
+Start the development server:
+
+bash
+Copy code
+npm start
+Open http://localhost:3000 in your web browser to view the application.
+
+Usage
+Enter the data for the QR code in the input field labeled "Data for Qrcode".
+Optionally, specify the image size in the input field labeled "Image size".
+Click the "Generate QR Code" button to generate the QR code dynamically.
+Click the "Download" button to download the generated QR code as a PNG image.
+Contributing
+Contributions are welcome! If you find any bugs or have suggestions for improvements, please open an issue or submit a pull request.
+
+License
+This project is licensed under the MIT License.
---
+++ src/Qrcode.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import "./index.css";
+import QRcode from './assets/QRcode.jpg'; 
+import { useState } from 'react';
+
+const Qrcode = () => {
+    const [img, setImg] = useState(QRcode);
+    const [loading, setLoading] = useState(false);
+    const [qrData, setQrData] = useState("");
+    const [qrSize, setQrSize] = useState("");
+
+    function generateQR() {
+        setLoading(true);
+        try {
+            const url = `https://api.qrserver.com/v1/create-qr-code/?size=${qrSize}x${qrSize}&data=${qrData}`;
+            setImg(url);
+        } catch (error) {
+            console.error("Error generating QRcode", error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    function downloadQR() {
+        fetch(img)
+            .then((response) => response.blob())
+            .then((blob) => {
+                const link = document.createElement("a");
+                link.href = URL.createObjectURL(blob);
+                link.download = "Qrcode.png";
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            }).catch((error) => {
+                console.error("The error while downloading img", error);
+            });
+    }
+
+    return (
+        <div>
+            <div className='header'><h1><span>Great</span>QR</h1></div>
+            <div className='app-container'>
+                <h1>QR CODE GENERATOR</h1>
+                {loading && <p>Please wait ...</p>}
+                {img && <img src={img} alt="Qrcode" className="Qrcode" />}
+                <div className='inner-box'>
+                    <label htmlFor="dataInput" className='input-label'>Data for Qrcode:</label>
+                    <input type="text" value={qrData} id="dataInput" placeholder='Enter the Data for Qrcode' onChange={(e) => setQrData(e.target.value)} />
+                    <label htmlFor="sizeInput" className='input-label'>Image size (e.g., 200):</label>
+                    <input type="text" value={qrSize} id="sizeInput" placeholder='Enter the Image size' onChange={(e) => setQrSize(e.target.value)} />
+                    <button className='generate-btn' disabled={loading} onClick={generateQR}>Generate Qrcode</button>
+                    <button className='download-btn' onClick={downloadQR}>Download</button>
+                </div>
+                <div className='footer'>
+                    <p>Designed By <span className='footer-span'>Sanjay</span> </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Qrcode;
